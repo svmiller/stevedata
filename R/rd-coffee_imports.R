0623df8889f5b0973e78d6b1fb881266fe09e9ef
svmiller <steven.v.miller@gmail.com>
2020-10-26 13:00:44 -0400

    add coffee data

---
+++ R/rd-coffee_imports.R
@@ -0,0 +1,17 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Coffee Imports for Select Importing Countries
+#'
+#' A simple time series on coffee imports for select importing countries (i.e. European Union + Japan + Russia + Tunisia + United States).
+#'
+#' @format A data frame with 29 observations on the following 3 variables.
+#' \describe{
+#' \item{\code{year}}{the year}
+#' \item{\code{imports}}{coffee imports for all select importing countries (in thousand 60-kg bags)}
+#' \item{\code{usaimports}}{coffee imports for just the United States (in thousand 60-kg bags)}
+#' }
+#'
+#' @details Data come from the International Coffee Organization, of which I feel I should be a member.
+#'
+"coffee_imports"
