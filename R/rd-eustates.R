ec7f0631275af9064ee7a6b8cbbeac00779aab31
svmiller <steven.v.miller@gmail.com>
2020-10-27 11:22:20 -0400

    add EU states

---
+++ R/rd-eustates.R
@@ -0,0 +1,17 @@
+#' @importFrom tibble tibble
+NULL
+
+#' EU Member States (Current as of 2019)
+#'
+#' European Union membership by accession date
+#'
+#' @format A data frame with 28 observations on the following 3 variables.
+#' \describe{
+#' \item{\code{date}}{a date indicating accession}
+#' \item{\code{country}}{a character vector for the country}
+#' \item{\code{iso2c}}{a character vector for iso2c}
+#' }
+#'
+#' @details Data come from \url{https://europa.eu/european-union/about-eu/countries_en}.
+#'
+"eustates"
