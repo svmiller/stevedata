40963d23e5c7cd26c7fb4dd5374d2cbcfb3d6420
svmiller <steven.v.miller@gmail.com>
2020-10-31 11:00:54 -0400

    add mcaddadd

---
+++ R/rd-steves_clothes.R
@@ -0,0 +1,24 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Steve's (Professional) Clothes, as of March 3, 2019
+#'
+#' I cobbled together this data set of the professional clothes
+#' (polos, long-sleeve dress shirts, pants) in my closet, largely for
+#' illustration on the origins of apparel in the U.S. for an intro lecture on trade.
+#'
+#' @format A data frame with 24 observations on the following 3 variables.
+#' \describe{
+#' \item{\code{type}}{Type of clothing}
+#' \item{\code{brand}}{The brand of clothing (e.g. Apt. 9, Saddlebred)}
+#' \item{\code{color}}{the color (and/or pattern) of the article of clothing}
+#' \item{\code{origin}}{The country that produced the garment.}
+#' }
+#'
+#' @details If you must know, I do most of my clothes shopping at major retailers
+#' in the U.S. (mostly Belk, J.C. Penney, and Kohl's). If that's you as well, the
+#' odds are good the distribution of my clothes will closely resemble yours.)
+#'
+#' @source Steve's closet. Hey, that's me!
+#'
+"steves_clothes"
