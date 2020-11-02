ae2cef5b7ad13903fc66580c0454ac52327b28fd
svmiller <steven.v.miller@gmail.com>
2020-11-02 14:17:16 -0500

    update, fix, and add.

--- R/rd-gss_abortion.R
+++ R/rd-gss_abortion.R
@@ -1,14 +1,15 @@
 #' @importFrom tibble tibble
 NULL
 
-#' Abortion Opinions in the General Social Survey (1972-2018)
+#' Abortion Opinions in the General Social Survey
 #'
 #' This is a toy data set derived from the General Social Survey that I intend
 #' to use for several purposes. First, the battery of abortion items can serve as
 #' toy data to illustrate mixed effects modeling as equivalent to a
 #' one-parameter (Rasch) model. Second, I include some covariates to also do some
 #' basic regressions. I think abortion opinions are useful learning tools for
-#' statistical inference for college students.
+#' statistical inference for college students. Third, there's a time-series component
+#' as well for understanding how abortion attitudes have changed over time.
 #'
 #' @format A data frame with 64,814 observations on the following 18 variables.
 #' \describe{
@@ -21,9 +22,8 @@ NULL
 #' \item{\code{educ}}{how many years the respondent spent in school}
 #' \item{\code{partyid}}{the respondent's party identification, as character variable}
 #' \item{\code{relactiv}}{the self-reported religious activity of the respondent on a 1:11 scale}
-#' \item{\code{abany}}{a numeric vector that equals 1 if the respondent thinks abortion
-#' should be legal for any reason. 0 indicates no support for
-#' abortion in this circumstance.}
+#' \item{\code{abany}}{a binary variable that equals 1 if the respondent thinks abortion
+#' should be legal for any reason. 0 indicates no support for abortion for any reason.}
 #' \item{\code{abdefect}}{a numeric vector that equals 1 if the respondent thinks
 #'  abortion should be legal if there is a serious defect in the fetus.
 #'  0 indicates no support for abortion in this circumstance.}
@@ -39,11 +39,11 @@ NULL
 #' \item{\code{abrape}}{a numeric vector that equals 1 if the respondent thinks
 #' abortion should be legal if the woman became pregnant because of a rape.
 #' 0 indicates no support for abortion in this circumstance.}
-#' \item{\code{absingle}}a numeric vector that equals 1 if the respondent thinks
+#' \item{\code{absingle}}{a numeric vector that equals 1 if the respondent thinks
 #' abortion should be legal if a pregnant woman is single and does not want to
 #' marry the man who impregnated her. 0 indicates no support for abortion
 #' in this circumstance.}
-#' \item{\code{pid7}}{\code{partyid} recoded so that 7 = NA}
+#' \item{\code{pid}}{\code{partyid} recoded so that 7 = NA}
 #' \item{\code{hispanic}}{a dummy variable that equals 1 if the respondent is any way Hispanic}
 #' }
 #'
