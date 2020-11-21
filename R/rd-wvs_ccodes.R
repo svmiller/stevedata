5eeb94de33203375c5327612043323de2b75442c
svmiller <steven.v.miller@gmail.com>
2020-11-21 10:46:28 -0500

    add another. don't know why the README isn't working on the laptop

---
+++ R/rd-wvs_ccodes.R
@@ -0,0 +1,19 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Syncing Word Values Survey Country Codes with CoW Codes
+#'
+#' A simple data set that syncs World Values Survey country codes (\code{s003})
+#'  with corresponding country codes from the Correlates of War state system
+#'  membership data.
+#'
+#' @format A data frame with 112 observations on the following 3 variables.
+#' \describe{
+#' \item{\code{s003}}{the World Values Survey country code}
+#' \item{\code{country}}{a character vector for the corresponding country name}
+#' \item{\code{ccode}}{the equivalent country code from the Correlates of War state system memberhsip data}
+#' }
+#'
+#' @details \url{http://svmiller.com/blog/2015/06/syncing-word-values-survey-country-codes-with-cow-codes/}
+#'
+"wvs_ccodes"
