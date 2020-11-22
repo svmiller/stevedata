94d1ef3b2f9ca0c78265c0c2e76d0b20eb0ac13a
svmiller <steven.v.miller@gmail.com>
2020-11-22 09:01:36 -0500

    another one

---
+++ R/rd-Guber99.R
@@ -0,0 +1,23 @@
+#' @importFrom tibble tibble
+NULL
+
+#' School Expenditures and Test Scores for 50 States, 1994-95
+#'
+#' A data set for a canonical case of a Simpson's paradox, useful for in-class
+#'  instruction on the topic.
+#'
+#' @format A data frame with 50 observations on the following 8 variables.
+#' \describe{
+#' \item{\code{state}}{a character vector for the state}
+#' \item{\code{expendpp}}{a numeric vector for the current expenditure per pupil in average daily attendance in public elementary and secondary schools, 1994-95 (in thousands of dollars)}
+#' \item{\code{ptratio}}{a numeric vector for the average pupil/teacher ratio in public elementary and secondary schools, Fall 1994}
+#' \item{\code{tsalary}}{a numeric vector for the estimated average annual salary of teachers in public elementary and secondary schools, 1994-95 (in thousands of dollars)}
+#' \item{\code{perctakers}}{a numeric vector for the percentage of all eligible students taking the SAT, 1994-95}
+#' \item{\code{verbal}}{a numeric vector for the average verbal SAT score, 1994-95}
+#' \item{\code{math}}{a numeric vector for the average math SAT score, 1994-95}
+#' \item{\code{total}}{a numeric vector for the average total SAT score, 1994-95}
+#' }
+#'
+#' @references Guber, Deborah Lynne. 1999. "Getting What You Pay For: The Debate Over Equity in Public School Expenditures." \emph{Journal of Statistics Education} 7(2).
+#'
+"Guber99"
