f2ce912903573153b0e4196a9b22c9bd6c743af9
svmiller <steven.v.miller@gmail.com>
2021-02-21 16:31:19 -0500

    fix that error

--- R/rd-gss_wages.R
+++ R/rd-gss_wages.R
@@ -15,7 +15,7 @@ NULL
 #' \item{\code{occrecode}}{recode of the occupation code into one of 11 main categories}
 #' \item{\code{prestg10}}{respondent's occupational prestige score (2010)}
 #' \item{\code{childs}}{number of children (0-8)}
-#' \item{\code{wrkstat}}the work status of the respondent (full-time, part-time, temporarily not working, unemployed (laid off), retired, school, housekeeper, other)}
+#' \item{\code{wrkstat}}{the work status of the respondent (full-time, part-time, temporarily not working, unemployed (laid off), retired, school, housekeeper, other)}
 #' \item{\code{gender}}{respondent's gender (male or female)}
 #' \item{\code{educcat}}{respondent's degree level (Less Than High School, High School, Junior College, Bachelor, or Graduate)}
 #' \item{\code{maritalcat}}{respondent's marital status (Married, Widowed, Divorced, Separated, Never Married)}
