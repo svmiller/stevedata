45ac47991cbed0c15fe359a1e6350f5459e81113
svmiller <steven.v.miller@gmail.com>
2021-04-05 15:48:57 -0400

    safdsaf

--- R/rd-election_turnout.R
+++ R/rd-election_turnout.R
@@ -6,7 +6,7 @@ NULL
 #' A simple data set on education and state-level (+ DC) turnout in the 2016
 #'  presidential election. This is inspired by what Pollock (2012) does in his book.
 #'
-#' @format A data frame with 51 observations on the following 10 variables.
+#' @format A data frame with 51 observations on the following 13 variables.
 #' \describe{
 #' \item{\code{year}}{the year of the presidential election (2016)}
 #' \item{\code{state}}{the state abbreviation}
@@ -19,11 +19,14 @@ NULL
 #' \item{\code{ss}}{is it a ``swing state?''}
 #' \item{\code{trumpw}}{did Trump win the state?}
 #' \item{\code{trumpshare}}{the share of the vote Trump received}
+#' \item{\code{sunempr}}{the state-level unemployment rate entering Nov. 2016}
+#' \item{\code{sunempr12md}}{the state-level unemployment rate (12-month difference) entering Nov. 2016}
 #' }
 #'
 #' @details Data were created in early 2017 for an upper-division course on
 #' quantitative methods. Educational attainment and division/region data come from the
 #' Census. Voter turnout/share data come from the Elections Project at George Mason University.
-#' GDP per capita estimates come from Bureau of Economic Analysis.
+#' GDP per capita estimates come from Bureau of Economic Analysis. Unemployment data come from
+#' the Bureau of Labor Statistics and code to generate it was derived from a forthcoming publication of mine.
 #'
 "election_turnout"
