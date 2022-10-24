292044a91c277351e0cad0c8f8b3f683178d1ea8
svmiller <steven.v.miller@gmail.com>
2022-10-24 12:40:44 +0200

    add a new data set, update turnips, file compression

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,16 @@
+# stevedata 0.8.0
+
+The newest release will have some new and updated data sets. Additions include:
+
+- `inglehart03`: a data set cobbled from Inglehart's (2003) "How Solid is Mass Support for Democracy---And How Can We Measure It?" in *PS: Politial Science & Politics*. These would include the information as presented in the article itself as opposed to the underlying (raw) data as one might get them in an earlier version of the World/European Survey.
+
+
+Updates include:
+
+- new turnip prices in `turnips`.
+- Changed file compression of files. where appropriate.
+
+
 # stevedata 0.7.0
 
 The newest release will have some new and updated data sets. Additions include:
---
+++ R/rd-inglehart03.R
@@ -0,0 +1,23 @@
+#' @importFrom tibble tibble
+NULL
+
+#' "How Solid is Mass Support for Democracy---And How Can We Measure It?"
+#'
+#' A data set based on summary information provided in Inglehart's (2003)
+#'  article in *PS: Political Science & Politics*. These data would be from
+#'  the article itself and only indirectly from the raw World or European
+#'  Values Survey.
+#'
+#' @format A data frame with 77 observations on the following 4 variables.
+#' \describe{
+#' \item{\code{state_year}}{the state year and survey year, as provided in the article}
+#' \item{\code{havedem}}{the percentage of respondents saying having a democratic political system is "very good" or "good"}
+#' \item{\code{strongleader}}{the percentage of respondents saying having a strong leader unencumbered by elections or parliaments is "very good" or "good"}
+#' \item{\code{muslim}}{a dummy variable that equals 1 if Inglehart codes the state as being a "predominently Islamic society"}
+#' }
+#'
+#' @details Data manually entered based on Table 1 and Table 2 in Inglehart's (2003) article.
+#'
+#' @references Inglehart, Ronald. 2003. "How Solid is Mass Support for Democracy---And How Can We Measure It?" *PS: Political Science & Politics* 36(1): 51--57.
+#'
+"inglehart03"
